4ec9eab32ae263dea9cc8daf7429c373445142f5
MuYu <mr.muzea@gmail.com>
2021-03-23 17:12:26 +0000

    build: move to go 1.16

--- build-release.sh
+++ build-release.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -ex
 BUILD_DIR=$(dirname "$0")/build
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
@@ -7,9 +8,10 @@ VERSION=`date -u +%Y%m%d`
 LDFLAGS="-X main.VERSION=$VERSION -s -w"
 GCFLAGS=""
 BINNAME="go-shadowsocks2"
-REPO="github.com/shadowsocks/go-shadowsocks2"
+REPO="https://github.com/shadowsocks/go-shadowsocks2.git"
 
-go get $REPO
+git clone $REPO
+cd $BINNAME
 
 # AMD64
 OSES=(linux)
@@ -19,10 +21,12 @@ for os in ${OSES[@]}; do
 	then
 		suffix=".exe"
 	fi
-	env CGO_ENABLED=0 GOOS=$os GOARCH=amd64 go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ${BINNAME}_${os}_amd64${suffix} $REPO
+	env CGO_ENABLED=0 GOOS=$os GOARCH=amd64 go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ${BINNAME}_${os}_amd64${suffix}
 	tar -zcf ${BINNAME}-${os}-amd64-$VERSION.tar.gz ${BINNAME}_${os}_amd64${suffix}
+	mv ${BINNAME}-${os}-amd64-$VERSION.tar.gz ..
 done
 
 # ARM64
-env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ${BINNAME}_linux_arm64  $REPO
+env CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -ldflags "$LDFLAGS" -gcflags "$GCFLAGS" -o ${BINNAME}_linux_arm64
 tar -zcf ${BINNAME}-linux-arm64-$VERSION.tar.gz ${BINNAME}_linux_arm64
+mv ${BINNAME}-linux-arm64-$VERSION.tar.gz ..
